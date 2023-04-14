75a0469bd2ece36d47a925edf23c33a4ff8e351b
Simon-2022 <96212730+Simon-2022@users.noreply.github.com>
2023-04-14 20:29:00 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video = "";
 status = "";
+objects = [];
 
 function preload() {
     video = createVideo('video.mp4');
@@ -13,6 +14,34 @@ function setup() {
 
 function draw() {
     image(video, 0, 0, 480, 380);
+    if(status != "")
+    {
+        objectDetector.detect(video, gotResult);
+        for(i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "status : objects detected :)";
+            document.getElementById("number_of_objects").innerHTML = "number of objects detected are : " + objects.length + " :)";
+            fill("blue");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("black");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    else
+    {
+        console.log(results);
+        objects = results;
+    }
 }
 
 function start() {
